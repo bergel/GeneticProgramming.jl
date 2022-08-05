b1ec940d5594ebe880e9f48a0d52e4434e63804b
Alexandre Bergel <alexandre.bergel@relational.ai>
2022-08-05 14:20:11 +0200

    added printing...

--- src/GeneticProgramming.jl
+++ src/GeneticProgramming.jl
@@ -4,15 +4,20 @@ using Infiltrator
 
 export GPNode
 export number_of_children, gp_type, gp_value, gp_children
-export gp_eval
+export gp_eval, gp_print
 
 struct GPNode
     type::Symbol
     value::Any
     children::Vector{GPNode}
+    print::Function
 
-    function GPNode(type::Symbol=:UNDEFINED, value::Any=nothing, children::Vector{GPNode}=GPNode[])
-        return new(type, value, children)
+    function GPNode(
+            type::Symbol=:UNDEFINED,
+            value::Any=nothing,
+            children::Vector{GPNode}=GPNode[],
+            printing::Function=gp_default_printing)
+        return new(type, value, children, printing)
     end
 end
 
@@ -20,6 +25,23 @@ gp_type(n::GPNode) = n.type
 gp_value(n::GPNode) = n.value
 gp_children(n::GPNode) = n.children
 number_of_children(n::GPNode) = length(gp_children(n))
+
+# Return a collection of strings
+function gp_default_printing(n::GPNode, res::Vector{String})
+    #@infiltrate
+    if(!(gp_value(n) isa Function))
+        push!(res, string(gp_value(n)))
+        return
+    end
+
+    push!(res, string(gp_type(n)))
+    push!(res, "( ")
+    for tuple in enumerate(gp_children(n))
+        gp_print(tuple[2], res)
+        (tuple[1] < number_of_children(n)) && push!(res, ", ")
+    end
+    push!(res, " )")
+end
 
 function gp_eval(n::GPNode)
     return gp_eval(n::GPNode, Dict{Symbol,Any}())
@@ -32,6 +54,16 @@ function gp_eval(n::GPNode, context::Dict{Symbol,Any})
     else
         return gp_value(n)
     end
+end
+
+function gp_print(n::GPNode)
+    res = String[]
+    gp_print(n, res)
+    return join(res)
+end
+
+function gp_print(n::GPNode, res::Vector{String})
+    n.print(n, res)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -33,6 +33,57 @@ using Test
 
         n_mult = GPNode(:Mult, *, [n_add, GPNode(:Number, 10)])
         @test gp_eval(n_mult) == 420
+
+        @test gp_print(n_mult) == "Mult( Addition( 40, 2 ), 10 )"
+    end
+
+    @testset "Hybrid printing" begin
+        n_number1 = GPNode(:Number, 40)
+        n_number2 = GPNode(:Number, 2)
+
+        function gp_print_add(n::GPNode, res::Vector{String})
+            push!(res, "(")
+            gp_print(gp_children(n)[1], res)
+            push!(res, " + ")
+            gp_print(gp_children(n)[2], res)
+            push!(res, ")")
+        end
+        n_add = GPNode(:Addition, +, [n_number1, n_number2], gp_print_add)
+        @test number_of_children(n_add) == 2
+        @test gp_children(n_add) == [n_number1, n_number2]
+        @test gp_value(n_add) == +
+
+        # evaluation
+        @test gp_eval(n_number1) == gp_value(n_number1)
+        @test gp_eval(n_number2) == gp_value(n_number2)
+        @test gp_eval(n_add) == 42
+
+        n_mult = GPNode(:Mult, *, [n_add, GPNode(:Number, 10)])
+        @test gp_eval(n_mult) == 420
+
+        @test gp_print(n_mult) == "Mult( (40 + 2), 10 )"
+    end
+
+    @testset "Printing" begin
+        n_number1 = GPNode(:Number, 40)
+        n_number2 = GPNode(:Number, 2)
+        function gp_print_add(n::GPNode, res::Vector{String})
+            push!(res, "(")
+            gp_print(gp_children(n)[1], res)
+            push!(res, " + ")
+            gp_print(gp_children(n)[2], res)
+            push!(res, ")")
+        end
+        function gp_print_mult(n::GPNode, res::Vector{String})
+            gp_print(gp_children(n)[1], res)
+            push!(res, " * ")
+            gp_print(gp_children(n)[2], res)
+        end
+        n_add = GPNode(:Addition, +, [n_number1, n_number2], gp_print_add)
+        n_mult = GPNode(:Mult, *, [n_add, GPNode(:Number, 10)], gp_print_mult)
+        @test gp_eval(n_mult) == 420
+
+        @test gp_print(n_mult) == "(40 + 2) * 10"
     end
 end
 
